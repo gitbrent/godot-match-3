4d915c8534c57b740f92ed5273eab3a8c2f90299
GitBrent <gitbrent@users.noreply.github.com>
2024-05-15 20:28:56 -0500

    WIP

--- game_boards/board_space/game_board/GemCell.gd
+++ game_boards/board_space/game_board/GemCell.gd
@@ -17,12 +17,12 @@ const DROP_OFFSET:int = 128 # (the sprite is centered in the 128x128 container, 
 var gem_color:Enums.GemColor
 # Declare and preload textures
 var gem_textures: Dictionary = {
-	Enums.GemColor.WHITE: preload("res://assets/gems/characters_0001.png"),
-	Enums.GemColor.RED: preload("res://assets/gems/characters_0002.png"),
-	Enums.GemColor.YELLOW: preload("res://assets/gems/characters_0003.png"),
-	Enums.GemColor.GREEN: preload("res://assets/gems/characters_0005.png"),
-	Enums.GemColor.BROWN: preload("res://assets/gems/characters_0006.png"),
-	Enums.GemColor.PURPLE: preload("res://assets/gems/characters_0007.png")
+	Enums.GemColor.WHITE: preload("res://assets/gems/space/gem1.png"),
+	Enums.GemColor.RED: preload("res://assets/gems/space/gem2.png"),
+	Enums.GemColor.YELLOW: preload("res://assets/gems/space/gem3.png"),
+	Enums.GemColor.GREEN: preload("res://assets/gems/space/gem4.png"),
+	Enums.GemColor.BROWN: preload("res://assets/gems/space/gem5.png"),
+	Enums.GemColor.PURPLE: preload("res://assets/gems/space/gem6.png")
 }
 
 func initialize(colorIn: Enums.GemColor):
