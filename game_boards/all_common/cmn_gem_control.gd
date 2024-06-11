f515b251cb4ebd26ce5aad7d78c1431d86735071
Brent Ely <gitbrent@users.noreply.github.com>
2024-06-10 22:00:24 -0500

    fixes for iOS

--- game_boards/all_common/cmn_gem_control.gd
+++ game_boards/all_common/cmn_gem_control.gd
@@ -8,6 +8,7 @@ signal drag_in_prog(gem_cell, mouse_position)
 signal drag_ended(gem_cell)
 
 # HANDLE mouse clicks/drags
+# NOTE: `event.global_position` is for mouse events and `event.position` is for touch events
 func _gui_input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed:
@@ -20,8 +21,10 @@ func _gui_input(event):
 	elif event is InputEventScreenTouch:
 		if event.pressed:
 			emit_signal("cell_click", get_parent())
-			emit_signal("drag_start", get_parent(), event.global_position)
+			emit_signal("drag_start", get_parent(), event.position)
 		else:
-			emit_signal("drag_ended", get_parent(), event.global_position)
+			emit_signal("drag_ended", get_parent(), event.position)
 	elif event is InputEventScreenDrag:
-		emit_signal("drag_in_prog", get_parent(), event.global_position)
+		# Use touch delta for smoother dragging
+		var touch_delta = event.position - event.get_position_in_parent(self) 
+		emit_signal("drag_in_prog", get_parent(), touch_delta)
